bcb7fde98936593d14ababb19de16e3603c27d56
Rahul Pandya <rahul25796@gmail.com>
2020-06-14 00:19:01 -0400

    updated moble view

--- public/js/main.js
+++ public/js/main.js
@@ -195,9 +195,18 @@ socket.on("user disconnected", (data) => {
 
 // for window resize
 
+// fixing height for mobile devices
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
 window.addEventListener("resize", onResize, false);
 // make the canvas fill its parent
 function onResize() {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
